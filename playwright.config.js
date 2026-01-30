5db80ee3bd0c4b2ac3c67603766800b271d2c3c6
lukaszdudzinski <lukasz.dudzinski@gmail.com>
2026-01-30 20:25:03 +0100

    fix(ci): explicitly skip visual tokens in playwright config for CI

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     testDir: './tests',
     testMatch: '**/*.spec.js',
-    testIgnore: 'unit/**',
+    testIgnore: process.env.CI ? ['**/visual_*.spec.js', 'unit/**'] : 'unit/**',
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
