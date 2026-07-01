1acaa4f235926b2eb28689f03bdd63c116d26585
Łukasz Dudziński <lukaszdudzinski@MacBook-Air-ukasz.local>
2026-07-01 20:38:35 +0200

    fix: Resolve GitHub Actions timeout by binding to 127.0.0.1 instead of localhost

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://localhost:8081',
+    baseURL: 'http://127.0.0.1:8081',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -73,8 +73,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx http-server -p 8081 -c-1',
-    url: 'http://localhost:8081/index.html',
+    command: 'npx http-server -a 127.0.0.1 -p 8081 -c-1',
+    url: 'http://127.0.0.1:8081/index.html',
     reuseExistingServer: !process.env.CI,
   },
 });
