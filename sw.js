ec2621142a3c224b5595974fe7b8dcf396f9be43
Łukasz Dudziński <lukaszdudzinski@github.com>
2026-07-31 13:53:32 +0200

    style: update Lunar Divers tiles text and remove turquoise left border

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ukis-dive-tools-v2026.7.31.05'; // Release v2026.7.31.05
+const CACHE_NAME = 'ukis-dive-tools-v2026.7.31.06'; // Release v2026.7.31.06
 // Core assets that MUST be cached immediately
 const CORE_ASSETS = [
     './',
