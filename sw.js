58d3d55a38dae126bb4426d334b3dcc2d06e3743
Łukasz Dudziński <lukaszdudzinski@github.com>
2026-07-31 13:36:27 +0200

    style: use logo_lunar.svg instead of png for Lunar Divers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ukis-dive-tools-v2026.7.31.03'; // Release v2026.7.31.03
+const CACHE_NAME = 'ukis-dive-tools-v2026.7.31.04'; // Release v2026.7.31.04
 // Core assets that MUST be cached immediately
 const CORE_ASSETS = [
     './',
